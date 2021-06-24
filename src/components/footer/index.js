6b64108521587aaa4057e5fb3f313f1fd42020d4
William Farnsworth <williamcoreyf@gmail.com>
2021-06-24 16:26:07 -0400

    added buttons to the footer and reworked the navbar buttons

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -5,11 +5,14 @@ const Footer = () => {
     return(
 
         <div className="p-1 text-center bg-dark">
-
+            <div className="p-1 text-center bg-dark">
+                <a href = "https://github.com/Cynwise"><button type="button" className="btn btn-light">GitHub</button></a>
+                <a href = "https://www.linkedin.com/in/williamcfarnsworth/"><button type="button" className="btn btn-light">LinkedIn</button></a>
+                <a href = "mailto:williamcoreyf@gmail.com"><button type="button" className="btn btn-light">Email</button></a>
+                <a href = "https://drive.google.com/file/d/19aOWWgUQCJIhXnWWOUFsr4ujc7pcfThy/view?usp=sharing"><button type="button" className="btn btn-light">Resume</button></a>
+            </div>
             <p className = "mb-3 text-light">Made by William Farnsworth</p>
-
         </div>
-    )
-}
+    )}
 
 export default Footer
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,22 +4,19 @@ const Header = () => {
 
     return(
 
-        <div className="p-3 text-center bg-dark">
+        <div className="p-3 text-center bg-dark row-12">
 
             <h1 className = "mb-3 text-light">William Farnsworth</h1>
             <h4 className = "mb-2 text-light">Web Developer</h4>
 
             <nav className="navbar navbar-expand-lg navbar-light bg-dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="">Navbar</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className=" collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav">
-                        <a className="nav-link active text-light" aria-current="page" href="">Home</a>
-                        <a className="nav-link active text-light" href="">Projects</a>
-                        <a className="nav-link active text-light" href="">Pricing</a>
+                <div className="container row" id="navbarNavAltMarkup">
+                    <div className="navbar-nav text-center col">
+                        <a className="nav-link active text-light max-vw-25" href="">About Me</a>
+                    </div>
+                    <div className="navbar-nav text-center col">
+                        <a className="nav-link active text-light max-vw-25" href="">Projects</a>
                     </div>
                 </div>
             </div>
