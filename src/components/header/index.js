c4f617ce6eda97190258032eb5ea6f7324933086
William Farnsworth <williamcoreyf@gmail.com>
2021-06-24 10:26:44 -0400

    added different source file and npm i bootstrap

---
+++ src/components/header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+
+    return(
+
+        <div className="p-5 text-center bg-dark">
+
+            <h1 className = "mb-3 text-light">William Farnsworth</h1>
+            <h4 className = "mb-3 text-light">Web Developer</h4>
+        </div>
+    )
+}
+
+export default Header
